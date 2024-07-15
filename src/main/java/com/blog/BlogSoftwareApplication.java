8f392b8614509fb5af423089883416f72ef5af67
Leonardo Braga <leonardobarrosbraga@gmail.com>
2024-07-15 14:44:33 -0300

    Initial commit

---
+++ src/main/java/com/blog/BlogSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSoftwareApplication.class, args);
+	}
+
+}
