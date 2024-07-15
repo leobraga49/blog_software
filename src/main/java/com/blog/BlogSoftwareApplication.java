4f834f9e633a4720934af4c1b7097dae9844f24c
Leonardo Braga <leonardobarrosbraga@gmail.com>
2024-07-15 14:59:58 -0300

    Step one done

--- src/main/java/com/blog/BlogSoftwareApplication.java
+++ src/main/java/com/blog/BlogSoftwareApplication.java
@@ -2,6 +2,8 @@ package com.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BlogSoftwareApplication {
@@ -10,4 +12,21 @@ public class BlogSoftwareApplication {
 		SpringApplication.run(BlogSoftwareApplication.class, args);
 	}
 
+	@RestController
+	public static class BlogController{
+
+		@GetMapping("/")
+		public String home() {
+			return "<!DOCTYPE html>\n" +
+					"<html lang=\"en\">\n" +
+					"  <head>\n" +
+					"    <title>My Blog</title>\n" +
+					"  </head>\n" +
+					"  <body>\n" +
+					"    <h1>My First Blog Post</h1>\n" +
+					"    <p>The first blog post I've written on my blogging software!</p>\n" +
+					"  </body>\n" +
+					"</html>";
+		}
+	}
 }
