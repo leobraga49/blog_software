a54c9b0e6e0e0db4a3e70c14ef8d01099514b769
Leonardo Braga <leonardobarrosbraga@gmail.com>
2024-07-18 22:10:13 -0300

    Refactor Post class to generate the table on DB and include publishedDate attribute

--- src/main/java/com/blog/model/Post.java
+++ src/main/java/com/blog/model/Post.java
@@ -1,22 +1,50 @@
 package com.blog.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+    @JsonProperty("id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @JsonProperty("title")
     private String title;
     @JsonProperty("summary")
     private String summary;
     @JsonProperty("content")
     private String content;
+    @JsonProperty("publishedDate")
+    private LocalDateTime publishedDate;
 
-    public Post(String title, String summary, String content) {
+    public Post(String title, String summary, String content, LocalDateTime publishedDate) {
         this.title = title;
         this.summary = summary;
         this.content = content;
+        this.publishedDate = publishedDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Post() {
+    }
+
+    public LocalDateTime getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDateTime publishedDate) {
+        this.publishedDate = publishedDate;
     }
 
     public String getTitle() {
