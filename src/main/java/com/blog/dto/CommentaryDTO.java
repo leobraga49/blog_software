b8805ea112e41524e198753388369d1ad36a4ab1
Leonardo Braga <leonardobarrosbraga@gmail.com>
2024-07-24 19:41:14 -0300

    Add CommentaryDTO for data transfer between layers

---
+++ src/main/java/com/blog/dto/CommentaryDTO.java
@@ -0,0 +1,50 @@
+package com.blog.dto;
+
+public class CommentaryDTO {
+
+    private Long id;
+    private String content;
+    private String publishedDate;
+    private String name;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
